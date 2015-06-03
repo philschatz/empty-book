a0156c81bdb9d7cd15012f5cf3e87f7fa7eb7fb4
Philip Schatz <phil@cnx.org>
2015-06-03 09:53:19 -0400

    add git commit to script

--- do.sh
+++ do.sh
@@ -15,26 +15,31 @@ REPO_PATH=../${REPO_NAME}
 
 mkdir -p ${TEMP_DIR}
 
-# Find the latest version
+echo "Find the latest version"
 curl --location http://archive.cnx.org/extras/${UUID} > ${JSON_FILE}
 
-# Find the URL for the latest version of the ZIP
+echo "Find the URL for the latest version of the ZIP"
 ZIP_URL=$(cat ${JSON_FILE} | jq --raw-output '.downloads | map(select(.format == "Offline ZIP")) | .[0].path')
 ZIP_URL="http://cnx.org${ZIP_URL}"
 
 
-# Follow redirects and download the zip file
+echo "Follow redirects and download the zip file"
 curl --location ${ZIP_URL} > ${ZIP_FILE}
 
-# Clear the dir before unzipping (so it does not prompt)
+echo "Clear the dir before unzipping (so it does not prompt)"
+echo "Clearing UUID_DIR"
 rm -rf ${UUID_DIR}
 
-# Unzip the file
+echo "Unzip the file"
 unzip ${ZIP_FILE} -d ${UUID_DIR}
 
-# The unzipped files are all in a dir named `col_*_complete`
+echo "The unzipped files are all in a dir named col_*_complete"
 COMPLETE_DIR=$(find ${UUID_DIR}/* | head -1)
 
-# sh ${ROOT}/convert-collection.sh ${COMPLETE_DIR} ${REPO_PATH}
-FOO=$(cd ${REPO_PATH} & git commit -m 'update book') # I wish I could specify the version here
+sh ${ROOT}/convert-collection.sh ${COMPLETE_DIR} ${REPO_PATH}
+
+echo "Commit the results"
+FOO=$(cd ${REPO_PATH} && git add --all . && git commit -m 'update book') # I wish I could specify the version here
+echo ${FOO}
+
 # git push ${REPO_PATH}
